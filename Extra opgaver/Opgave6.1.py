16c5774600e5d5f821f708790a3ef32e44fbe815
Nilas <nilas.b-s@outlook.dk>
2024-03-19 14:55:32 +0100

    Ekstra opgaver

---
+++ Extra opgaver/Opgave6.1.py
@@ -0,0 +1,12 @@
+range = 1000
+num1 = 0
+num2 = 1
+next_number = num2
+count = 1
+
+while count <= range:
+	print(next_number, end="\n")
+	count += 1
+	num1, num2 = num2, next_number
+	next_number = num1 + num2
+print()
