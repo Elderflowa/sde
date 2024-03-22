d4ec11b7291b4b7b6a41e73997ca54977a345b1c
Nilas <nilas.b-s@outlook.dk>
2024-03-22 10:10:25 +0100

    Ekstra opgaver

--- Extra opgaver/Opgave10.py
+++ Extra opgaver/Opgave10.py
@@ -1,5 +1,5 @@
 def listerMister ():
-    listInput = input("Enter a list of things seperated by a comma: ")
+    listInput = input("Enter a list of things seperated by a comma and a space \n(eks1, eks2): ")
     commas = listInput.count(",")
     createdList = list(listInput.split(" "))
     createdList.sort()
@@ -7,6 +7,6 @@ def listerMister ():
     if commas > 0:
         print(createdList)
     else:
-        print("Your input only has one element \nPlease input a list of multiple elements seperated by commas")
+        print("Your input only has one element \nPlease input a list of multiple elements seperated by a comma and a space")
         listerMister()
 listerMister()
